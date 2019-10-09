759b4201aa14ab8a0646432d0b452bee48a4d1f5
RocketGeek <support@rocketgeek.com>
2019-10-09 11:05:08 -0400

    initial commit

---
+++ rocketgeek-jquery-tabs.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Initializes RocketGeek_jQuery_Tabs class.
+ *
+ * @link URL
+ *
+ * @since 1.0.0
+ */
+	
+/**
+ * Load the class.
+ */
+include_once ( 'class-rocketgeek-jquery-tabs.php' );
+
+// Initialize the class.
+RocketGeek_jQuery_Tabs::init();
