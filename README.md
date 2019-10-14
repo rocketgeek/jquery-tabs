02c23aec2f9a07e3ae6a8f57ab0f3494f540fed0
RocketGeek <support@rocketgeek.com>
2019-10-14 09:20:38 -0400

    condense files, add license

---
+++ README.md
@@ -0,0 +1,75 @@
+# RocketGeek jQuery UI tabs for WordPress
+
+This is a code library for WordPress plugins. You can include this library according to the instructions and implement a set of jQuery tabs for your content.
+
+Currently, it is designed for use in admin panels, but eventually, it may add a frontend tool/shortcode/gutenberg block.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+It is assumed that you are already running WordPress.  If so, you just need to include this library in your plugin (or theme).
+
+The library relies on jQuery UI. It enqueues jQuery and jQuery UI Core using `wp_enqueue_script`.  This means that if you properly load these two libraries already in other plugins or your theme, then you're fine.  But if you use plugins or themes that improperly load these two libraries (i.e. they do not enqueue but rather load directly), you may run into conflicts.
+
+### Using the library
+
+Copy the entire rocketgeek-jquery-tabs folder to your project. I like to do this in an "includes" subdirectory. For example:
+
+```
+include_once( YOUR_PLUGIN_PATH . 'includes/libraries/rocketgeek-jquery-tabs/rocketgeek-jquery-tabs.php' );
+```
+
+Once the library is included, you can call it in your project. Define the tabs as an array, and pass that array with the call.
+
+```
+$tabs = array(
+	'my_first_tab' => array(
+		'tab' => 'First Tab Title',
+		'content' => 'The tab content (can be a variable if you build more complex HTML).',
+	),
+	'my_second_tab' => array(
+		'tab' => __( 'Second Tab Title (localized)', 'your-text-domain' ),
+		'content' => 'The tab content (can be a variable if you build more complex HTML).',
+	),
+);
+RocketGeek_jQuery_Tabs::tabs( $tabs );
+```
+
+The primary array keys ('my_first_tab', 'my_second_tab') can be whatever you need them to be.  The sub-array keys ('tab' and 'content') MUST be 'tab' and 'content'.
+
+The library accepts a couple of additional arguments beyond the `$tabs` definition.  You can give your tabs a unique ID (useful if implementing more than one set of tabs on a page), and you can toggle whether the generated HTML is echoed immediately (default) or returned as a variable.  For example:
+
+```
+$my_tabs = RocketGeek_jQuery_Tabs::tabs( $tabs, 'my_tabs', false );
+```
+
+In the example above, the tabs would be wrapped with div ID "my_tabs" and the generated HTML would be returned to the variable `$my_tabs` instead of echoed to the screen.
+
+
+## Built With
+
+* [jQuery](https://jquery.com/)
+* [jQuery UI](https://jqueryui.com/)
+* [WordPress](https://make.wordpress.org/)
+
+## Contributing
+
+I do accept pull requests. However, make sure your pull request is properly formatted. Also, make sure your request is generic in nature. In other words, don't submit things that are case specific - that's what forks are for. The library also has hooks that follow WP standards - use 'em.
+
+## Versioning
+
+I use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/rocketgeek/jquery_tabs/tags). 
+
+## Authors
+
+* **Chad Butler** - [ButlerBlog](https://github.com/butlerblog)
+* **RocketGeek** - [RocketGeek](https://github.com/rocketgeek)
+
+## License
+
+This project is licensed under the GPL v3 License - see the [LICENSE.md](LICENSE.md) file for details.
+
+I hope you find this project useful. If you use it your project, attribution is appreciated.
--- rocketgeek-jquery-tabs.php
+++ rocketgeek-jquery-tabs.php
@@ -1,16 +1,203 @@
 <?php
 /**
- * Initializes RocketGeek_jQuery_Tabs class.
+ * RocketGeek jQuery UI Tabs Library
  *
- * @link URL
+ * Provides a class to invoke jQuery UI tabs for use in WordPress
+ * plugins and themes.  See the readme.md for initial instructions.
+ *
+ * This library is open source and GPL licensed. I hope you find it useful
+ * for your project(s). Attribution is appreciated ;-)
+ *
+ * @package    {Your Project Name}
+ * @subpackage RocketGeek_jQuery_Tabs
+ * @version    1.0.0
+ *
+ * @link       https://github.com/rocketgeek/jquery_tabs/
+ * @author     Chad Butler <https://butlerblog.com>
+ * @author     RocketGeek <https://rocketgeek.com>
+ * @copyright  Copyright (c) 2019 Chad Butler
+ * @license    https://github.com/rocketgeek/jquery_tabs/blob/master/LICENSE.md GNU General Public License 3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Make sure the class is only loaded once.
+if ( ! class_exists( 'RocketGeek_jQuery_Tabs' ) ):
+/**
+ * The RocketGeek_jQuery_Tabs class.
+ *
+ * Provides a class to invoke jQuery UI tabs for WordPress plugins.
  *
  * @since 1.0.0
  */
+class RocketGeek_jQuery_Tabs {
 	
-/**
- * Load the class.
- */
-include_once ( 'class-rocketgeek-jquery-tabs.php' );
+	/**
+	 * Filter stem.
+	 *
+	 * @since 1.0.0
+	 * @param string
+	 */
+	public static $stem = "rktgk_";
+	
+	/**
+	 * Initialize the class.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function init() {
+		self::loader();
+	}
+	
+	/**
+	 * Load hooks.
+	 *
+	 * @since 1.0.0
+	 */
+	private static function loader() {
+		add_action( 'admin_enqueue_scripts', 'RocketGeek_jQuery_Tabs::enqueue' );
+	}
+	
+	/**
+	 * Enqueue jQuery
+	 *
+	 * @since 1.0.0
+	 */
+	public static function enqueue() {
+		wp_enqueue_script( 'jquery' );
+		wp_enqueue_script( 'jquery-ui-core' ); // enqueue jQuery UI Core
+		wp_enqueue_script( 'jquery-ui-tabs' ); // enqueue jQuery UI Tabs
 
-// Initialize the class.
+		/**
+		 * Filter the CSS file loaded.
+		 *
+		 * Allows you to load the non-minified CSS for debugging
+		 * and other purposes. Load the non-minified CSS OR a 
+		 * custom stylesheet.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param  string  $jquery_ui_style
+		 */
+		$jquery_ui_style = apply_filters( self::$stem . 'jquery_ui_style', plugin_dir_url( __FILE__ ) . 'assets/css/jquery-ui.min.css' );
+		wp_enqueue_style ( 'jquery-ui-style' );
+		
+		//wp_register_style( 'rktgk-ui-tabs', plugin_dir_url( __FILE__ ) . 'assets/css/rktgk-tabs.css' );
+		//wp_enqueue_style ( 'rktgk-ui-tabs' );
+	}
+	
+	/**
+	 * Invoke jQuery Tabs.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param  array   $tabs {
+	 *    @type string $tab     (required)
+	 *    @type string $content (optional)
+	 * }
+	 * @param  string  $tag (default:default)
+	 */
+	public static function tabs( $tabs, $tag = 'rktgk_tabs', $echo = true ) {
+		
+		$tag = ( '' == $tag ) ? 'rktgk_tabs' : $tag;
+		
+		/**
+		 * Load a set of tabs.
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param array {
+		 *    @type string $tab     (required)
+		 *    @type string $content (optional)
+		 * }
+		 * @param string $tag
+		 */
+		$tabs = apply_filters( self::$stem . 'jquery_tabs', $tabs, $tag ); 
+
+		if ( ! empty( $tabs ) ) { 
+			
+			$html = '
+<style>
+	#' . $tag . '{ 
+		padding: 0px; 
+		background: none; 
+		border-width: 0px; 
+	} 
+	#' . $tag . ' .ui-tabs-nav { 
+		padding-left: 0px; 
+		background: transparent; 
+		border-width: 0px 0px 1px 0px; 
+		-moz-border-radius: 0px; 
+		-webkit-border-radius: 0px; 
+		border-radius: 0px; 
+	} 
+	#' . $tag . ' .ui-tabs-panel { 
+		background: #fff; 
+		border-width: 0px 1px 1px 1px; 
+	}
+	#' . $tag . ' .ui-state-active {
+		border: 1px solid #006799; 
+		background: #008ec2;
+	}
+	#' . $tag . ' .ui-state-active a {
+		color: #fff;
+	}
+</style>
+<script>
+	jQuery(document).ready(function($){
+		$("#' . $tag . '").tabs();
+	});
+</script>';
+			$html .= '<div id="' . esc_attr( $tag ) . '">';
+			$html .= '<ul>';
+			foreach ( $tabs as $key => $value ) {
+				$id = "#" . $tag . "-" . $key;
+				$class = ( isset( $_GET['ui-tabs-active'] ) && $key == $_GET['ui-tabs-active'] ) ? ' class="ui-tabs-active ui-state-active"' : '';
+				$html .= '<li' . $class . '><a href="' . esc_attr( $id ) . '">' . esc_html( $value['tab'] ) . '</a></li>';
+			}
+			$html .= '</ul>';
+			foreach ( $tabs as $key => $value ) {
+				$id = $tag . "-" . $key;
+				$html .= '<div id="' . esc_attr( $id ) . '">';
+				
+				$content = ( isset( $value['content'] ) ) ? $value['content'] : '';
+				
+				/**
+				 * Fires after a tab's content.
+				 *
+				 * @since 1.0.0
+				 *
+				 * @param string $content
+				 * @param string $key
+				 * @param string $tag
+				 */
+				$html .= apply_filters( self::$stem . 'jquery_tabs_content', $content, $key, $tag );
+				
+				$html .= '</div>';
+			}
+			$html .= '</div>';
+			
+			if ( $echo ) {
+				echo $html;
+			} else {
+				return $html;
+			}
+		}
+	}
+}
+
+// Initialize the tabs library.
 RocketGeek_jQuery_Tabs::init();
+
+endif;
