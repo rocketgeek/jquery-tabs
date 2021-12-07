ff76963e99185b8b44fe5a0cb9e00138d7bd9769
RocketGeek <support@rocketgeek.com>
2021-12-07 15:36:44 -0500

    updated to apache 2 license

--- rocketgeek-jquery-tabs.php
+++ rocketgeek-jquery-tabs.php
@@ -5,31 +5,31 @@
  * Provides a class to invoke jQuery UI tabs for use in WordPress
  * plugins and themes.  See the readme.md for initial instructions.
  *
- * This library is open source and GPL licensed. I hope you find it useful
+ * This library is open source and Apache-2.0 licensed. I hope you find it useful
  * for your project(s). Attribution is appreciated ;-)
  *
- * @package    {Your Project Name}
- * @subpackage RocketGeek_jQuery_Tabs
+ * @package    RocketGeek_jQuery_Tabs
  * @version    1.0.0
  *
  * @link       https://github.com/rocketgeek/jquery_tabs/
  * @author     Chad Butler <https://butlerblog.com>
  * @author     RocketGeek <https://rocketgeek.com>
- * @copyright  Copyright (c) 2019 Chad Butler
- * @license    https://github.com/rocketgeek/jquery_tabs/blob/master/LICENSE.md GNU General Public License 3
+ * @copyright  Copyright (c) 2019-2021 Chad Butler
+ * @license    Apache-2.0
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright [2021] Chad Butler, RocketGeek
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 // Exit if accessed directly.
